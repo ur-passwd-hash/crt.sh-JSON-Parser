c601886d6b20cefe873f3442176c06fe85ced288
ur-pwd-hash <zachary.satterly@gmail.com>
2019-12-26 15:21:54 -0500

    Add files via upload

---
+++ crtsh_json_parser.py
@@ -0,0 +1,14 @@
+import json
+import urllib.request
+
+url_name = input("Enter the crt.sh url (don't forget to append '&output=JSON'):")
+
+with urllib.request.urlopen(url_name) as url:
+	data = json.loads(url.read().decode())
+	i = 0
+while i == i:
+	try:
+		print(data[i]['name_value'])
+		i=i+1
+	except (IndexError) as err:
+		pass
